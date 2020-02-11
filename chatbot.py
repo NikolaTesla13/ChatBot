f5524439b04542f0229bd271cf032f89258df9d7
Dumitru-Asandei <52822996+NikolaTesla13@users.noreply.github.com>
2020-02-11 21:03:33 +0200

    New version
    Now it can stock informations into a "brain"

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,4 @@
+
 def create_words(sentence):
     words =[]
     temp = ""
@@ -12,65 +13,116 @@ def create_words(sentence):
             temp=""
     return words
 
+sentence = input()
+words = create_words(sentence)
+
 def sentence_type(words):
     for i in words:
         if i.upper() == "WHAT" or i.upper() == "HOW" or i.upper() == "WHY" or i.upper() == "WHEN" or i.upper() == "WHERE":
             return "question"
         else:
-            return "command"
+            return "afirmation"
 
-def question_type(words):
-    for i in words:
-        if i.upper() == "WHAT":
-            return "thing"
-        if i.upper() == "HOW":
+
+if sentence_type(words) == "question":
+    def question_type(words):
+        for i in words:
+            if i.upper() == "WHAT":
+                return "thing"
+            if i.upper() == "HOW":
+                return "action"
+            if i.upper() == "WHY":
+                return "explain"
+            if  i.upper() == "WHEN":
+                return "time"
+            if i.upper() == "WHERE":
+                return "place"
+
+    def verb_type_error(words):
+        for i in words:
+            if i.upper() == "IS" or i.upper() == "ARE" or i.upper() == "WAS" or i.upper() == "WERE":
+                return "being"
+    def verb_type(words): 
+        if verb_type_error(words) == None:
             return "action"
-        if i.upper() == "WHY":
-            return "explain"
-        if  i.upper() == "WHEN":
-            return "time"
-        if i.upper() == "WHERE":
-            return "place"
-
-def verb_type_error(words):
-    for i in words:
-        if i.upper() == "IS" or i.upper() == "ARE" or i.upper() == "WAS" or i.upper() == "WERE":
+        else:
             return "being"
-def verb_type(words):
-    v = words[-1]
-    l = list(v)
-    l[-1] = ""
-    verb = "".join(l)
-    
-    if verb_type_error(words) == None:
-        return "action"
-    else:
-        return "being"
 
-def verb(words):
-    v = words[2]
-    l = list(v)
-    if l[-1] == "." or l[-1] == "?" or l[-1] == "!":
-        l[-1] = ""
-    verb = "".join(l)
-    return verb
+    def verb(words):
+        v = words[2]
+        l = list(v)
+        if l[-1] == "." or l[-1] == "?" or l[-1] == "!":
+            l[-1] = ""
+        verb = "".join(l)
+        return verb
 
-def subject(words):
-    return words[1]
+    def subject(words):
+        return words[1]
 
-def build_answer(verb, subject, question_type):
-    preposition = ""
-    if question_type == "explain":
-        preposition = "because"
-    #elif question_type == ... 
-        
-    answer = [subject, verb, preposition, "BETA"]
-    return answer
+    def answer(words):
+        brain = open("brain.txt","r+")
+        info = ""
+        data = brain.readlines()
+        for i in data:
+            row = list(i)
+            if row[0] == "i":
+                for j in range(11, len(row)-1):
+                    info = info + row[j]
+        return info
+
+
+
+    def build_answer(verb, subject, question_type, answer):
+        preposition = ""
+        if question_type == "explain":
+            preposition = "because"
+        #elif question_type == 
             
+        answer = [subject, verb, preposition, answer]
+        return answer   
 
-sentence = input()
-words = create_words(sentence)
-for i in build_answer(verb(words),subject(words), question_type(words)):
-    print(i,end=" ")
-    
-    
+    for i in build_answer(verb(words),subject(words), question_type(words), answer(words)):
+        print(i,end=" ")
+        
+else:
+
+        def subject(words):
+            return words[0]
+
+        def verb(words):
+            return words[1]
+
+        def preposition(words):
+            return words[2]
+
+        def information(words):
+            information = []
+            
+            for i in range(3, len(words)):
+                information.append(words[i])
+            string = ""
+            for j in information:
+                string = string + " " + j
+
+            return string
+        
+        def build_answer(subject, verb, preposition, information, words):
+            answer = [subject, verb, preposition, information]
+
+            try:
+                string_answer = ""
+                for i in answer:
+                    string_answer += i
+                return string_answer
+            except:
+                return answer
+
+        def memorize_answer(subject, verb, preposition, information):
+            brain = open("brain.txt","a+")
+            brain.write("subject: " + subject + "\nverb: " + verb + "\npreposition: "+ preposition +  "\ninformation: " + str(information) + "\n")
+            brain.close() 
+
+        for i in build_answer(subject(words), verb(words), preposition(words), information(words), words):
+            print(i, end=" ")
+
+        memorize_answer(subject(words), verb(words), preposition(words), information(words))
